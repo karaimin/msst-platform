482d38a521257ed9d4f6f3efc53064f69930920b
JustBePlayer <a.karaimin@gmail.com>
2018-11-08 22:35:44 +0200

    add spring boot to project

---
+++ src/main/java/com/msstp/msstparent/MsstParentApplication.java
@@ -0,0 +1,12 @@
+package com.msstp.msstparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsstParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsstParentApplication.class, args);
+	}
+}
